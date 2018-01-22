733a83bd0f908f849a93dc9ccbe73212845f58c9
Marcus Grant <marcus.grant@patternbuffer.io>
2018-01-21 23:30:21 -0500

    Integration of SASS into gatsby
    - Address issue #3
    - add gatsby-plugin-sass
    - delete old stylesheet
    - replace with index.scss as entry point for style
      - this is the typical main.scss used in other projects
    - Minor tweaks to layout files towards first release state

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 
 const IndexPage = () => (
   <div>
-    <h1>Pattern Buffer</h1>
+    <h1>Main Page</h1>
     <Link to="/page-2/">Go to page 2</Link>
     <br />
     <Link to="/page-3">Go to page 3</Link>
