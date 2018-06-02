7c308c85e17caa4ae762d5904c275ed06009b66c
Marcus Grant <marcusfg@gmail.com>
2018-06-02 19:14:46 -0400

    [#35] Partial of sidebar improvement
    * Sidebar got its own scss file
    * NavMenu created to manage rendering the nav menu
    * nav-menu.scss is the stylesheet for the menu
    * global stylesheet got fonts:
      * Montserrat
      * Merriweather
      * Open Sans
      * Source Code Pro
    * Now main font is Montserrat

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,58 +1,62 @@
 import React from 'react';
-import Link from 'gatsby-link';
 // import PropTypes from 'prop-types';
-// import '../../styles/bulma-composed.scss';
+
+import NavMenu from './nav-menu';
+
 import './sidebar.scss';
 
 import logo from './pattern-buffer-v2_1.svg';
 
+const navItems = [
+  { text: 'Feed', route: '/' },
+  { text: 'About', route: '/about' },
+  { text: 'Projects', route: '/projects' },
+];
+
 const Sidebar = () => (
   <div className="sidebar__container">
-    <figure className="image is-3by4">
-      <img src={logo} alt="logo" />
-    </figure>
-    <h1
-      style={{ color: 'white' }}
-    >Pattern Buffer
-    </h1>
-    <ul style={{ listStyle: 'none', float: 'right' }}>
-      <li style={{ display: 'inline-block', marginRight: '1rem' }}>
-        <Link
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            fontSize: 'medium',
-          }}
-          to="/"
-        >
-          Feed
-        </Link>
-      </li>
-      <li style={{ display: 'inline-block', marginRight: '1rem' }}>
-        <Link
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            fontSize: 'medium',
-          }}
-          to="/about"
-        >About
-        </Link>
-      </li>
-      <li style={{ display: 'inline-block', marginRight: '1rem' }}>
-        <Link
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            fontSize: 'medium',
-          }}
-          to="/projects"
-        >Projects
-        </Link>
-      </li>
-    </ul>
+    <img src={logo} alt="logo" />
+    <h1 className="site__title">PATTERN BUFFER</h1>
+    <NavMenu navItems={navItems} />
   </div>
 );
 
 
 export default Sidebar;
+
+// <ul style={{ listStyle: 'none', float: 'right' }}>
+//   <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+//     <Link
+//       style={{
+//         color: 'white',
+//         textDecoration: 'none',
+//         fontSize: 'medium',
+//       }}
+//       to="/"
+//     >
+//       Feed
+//     </Link>
+//   </li>
+//   <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+//     <Link
+//       style={{
+//         color: 'white',
+//         textDecoration: 'none',
+//         fontSize: 'medium',
+//       }}
+//       to="/about"
+//     >About
+//     </Link>
+//   </li>
+//   <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+//     <Link
+//       style={{
+//         color: 'white',
+//         textDecoration: 'none',
+//         fontSize: 'medium',
+//       }}
+//       to="/projects"
+//     >Projects
+//     </Link>
+//   </li>
+// </ul>
