38b800c67834f796afe7850e1dcda9defc7aec47
Marcus Grant <marcusfg@gmail.com>
2018-06-03 01:03:58 -0400

    [#35] Add hover effects to NavMenu

--- src/components/Wrappers/Sidebar/nav-menu.js
+++ src/components/Wrappers/Sidebar/nav-menu.js
@@ -27,24 +27,3 @@ NavMenu.propTypes = {
 };
 
 export default NavMenu;
-/*
-  <ul className="nav-menu__list">
-    {
-      navItems.map(navItem => (
-        <li
-          className="nav-menu__item"
-          key={`li-${navItem.text}`}
-        >
-          <Link
-            className="nav-menu__link"
-            activeClassName="nav-menu__link--active"
-            exact
-            to={navItem.route}
-            key={`a-${navItem.text}`}
-          >{navItem.text}
-          </Link>
-        </li>
-      ))
-    }
-  </ul>
-*/
