472c4d406583a448a7d9058edc601b06784334b8
Marcus Grant <marcusfg@gmail.com>
2018-06-29 17:41:43 -0400

    [#47] fix default outline access. focus on buttons,stil need alternative UI cue

--- src/components/Buttons/sidebar-toggle.js
+++ src/components/Buttons/sidebar-toggle.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SidebarToggle = ({ isActive, onMenuClick }) => (
-  <div
-    className="sidebar-toggle__wrapper"
-    onClick={onMenuClick}
-    onKeyPress={onMenuClick}
-    role="button"
-    tabIndex={0}
-  >
-    <div className={`animated-menu-icon${isActive ? ' animate' : ''}`} />
-  </div>
-);
+class SidebarToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.highlightButton = this.highlightButton.bind(this);
+    this.highlightButton = this.unHighlightButton.bind(this);
+    this.state = {
+      buttonHighligted: false,
+    };
+  }
+
+  highlightButton() {
+    tihs.setState({ buttonHighligted: true });
+  }
+
+  unHighlightButton() {
+    tihs.setState({ buttonHighligted: false });
+  }
+
+  handleMouseHover() {
+    this.setState(prevState => ({ isHovering: !prevState.isHovering }));
+  }
+
+  render() {
+    const classFromStateAndProps = () => {
+      return `animated-menu-icons${isActive ?
+          ' animate'
+      `;
+    };
+    return (
+      <div
+        className="sidebar-toggle__wrapper"
+        onClick={onMenuClick}
+        onKeyPress={onMenuClick}
+        role="button"
+        tabIndex={0}
+        onMouseEnter={this.highlightButton}
+        onMouseLeave={this.unHighlightButton}
+      >
+        <div className={`animated-menu-icon${isActive ? ' animate' : ''}`} />
+      </div>
+    );
+  }
+}
 
 SidebarToggle.propTypes = {
   isActive: PropTypes.bool,
@@ -23,3 +55,8 @@ SidebarToggle.defaultProps = {
 };
 
 export default SidebarToggle;
+
+
+// <div className="sidebar-toggle__wrapper" onClick={onMenuClick}>
+//   <div className={`animated-menu-icon${isActive ? ' animate' : ''}`} />
+// </div>
