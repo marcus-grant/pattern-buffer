7fc9fae704f43748fe262b005286f263c07a662f
Marcus Grant <marcusfg@gmail.com>
2018-07-30 17:30:54 -0400

    [#45] Give NavMenu seperate rendering logic using orientation prop
    * In order to properly fit NavMenu in top bar it needs to render horizontally
    * There's a render function for each orientation
    * then there's a function that picks which using switch
    * The functional componenet then just delegates roughly the same rendering to this switch case
    * Only slight differences in classnames then in each class's styles to make rendering a little better on small screens.

--- src/components/Wrappers/Menus/NavMenu/nav-menu.js
+++ src/components/Wrappers/Menus/NavMenu/nav-menu.js
@@ -2,22 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
+const horizontalNavMenu = navItems => navItems.map(navItem => (
+  <Link
+    className="nav-menu__link--horizontal"
+    activeClassName="nav-menu__link--active"
+    exact
+    to={navItem.route}
+    key={`a-${navItem.text}`}
+  >{navItem.text}
+  </Link>
+));
+
+const verticalNavMenu = navItems => navItems.map(navItem => (
+  <Link
+    className="nav-menu__link--vertical"
+    activeClassName="nav-menu__link--active"
+    exact
+    to={navItem.route}
+    key={`a-${navItem.text}`}
+  >{navItem.text}
+  </Link>
+));
+
+const errorPlaceholder = orientation => (
+  <h1 style={{ color: 'red' }}>
+    {`NavMenu Error! Orientation, ${orientation}, isnt a valid value`}
+  </h1>
+);
+
+const menuFromItemsAndOrientation = (navItems, orientation) => {
+  switch (orientation) {
+    case 'horizontal':
+      return horizontalNavMenu(navItems);
+    case 'vertical':
+      return verticalNavMenu(navItems);
+    default:
+      return errorPlaceholder(orientation);
+  }
+};
+
 const NavMenu = ({ navItems, orientation }) => (
   <div className={
     `nav-menu__list--${orientation === 'vertical' ? 'vertical' : 'horizontal'}`
   }
   >
     {
-      navItems.map(navItem => (
-        <Link
-          className="nav-menu__link"
-          activeClassName="nav-menu__link--active"
-          exact
-          to={navItem.route}
-          key={`a-${navItem.text}`}
-        >{navItem.text}
-        </Link>
-      ))
+      menuFromItemsAndOrientation(navItems, orientation)
     }
   </div>
 );
--- src/components/Wrappers/Menus/Topbar/topbar.js
+++ src/components/Wrappers/Menus/Topbar/topbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Media from 'react-media';
 
 import NavMenu from '../NavMenu/nav-menu';
 
